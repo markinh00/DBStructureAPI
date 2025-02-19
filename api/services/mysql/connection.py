20ee0f3ff8ee0b89c692803542d336c59dafa175
João Marcos Coutinho Nunes <jmcoutinhonunes@gmail.com>
2025-02-19 14:21:40 -0300

    feat: add connection to mysql database

--- api/services/mysql/connection.py
+++ api/services/mysql/connection.py
@@ -1,25 +1,31 @@
+import os
 import mysql.connector
 from mysql.connector import Error
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
 
 
-def connect_to_database():
-    try:
-        connection = mysql.connector.connect(
-            host="your_host",  # e.g., "localhost" or an IP address
-            user="your_username",  # e.g., "root"
-            password="your_password",  # your MySQL password
-            database="your_database"  # your database name
-        )
+class MySQL:
+    def __init__(self):
+        self.connection: PooledMySQLConnection | MySQLConnectionAbstract | None = None
 
-        if connection.is_connected():
-            print("Successfully connected to the database")
-            return connection
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
-        return None
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host=os.getenv("MYSQL_HOST"),
+                user=os.getenv("MYSQL_USER"),
+                password=os.getenv("MYSQL_PASSWORD"),
+                database=os.getenv("MYSQL_DATABASE")
+            )
 
+            if self.connection.is_connected():
+                print("Successfully connected to the database")
+                return self.connection
+        except Error as e:
+            print(f"Error while connecting to MySQL: {e}")
+            return None
 
-def close_connection(connection):
-    if connection and connection.is_connected():
-        connection.close()
-        print("MySQL connection closed")
+    def close_connection(self):
+        if self.connection and self.connection.is_connected():
+            self.connection.close()
+            print("MySQL connection closed")
